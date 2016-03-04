411964c3ed7e328f3fe5e087cc334593e0019c65
Andrey Kovtun <kovtunos@openmailbox.org>
2016-03-04 12:01:29 +0300

    Rewrite hello app. It works.

--- hello.py
+++ hello.py
@@ -1,14 +1,17 @@
-import urlparse
+from urlparse import parse_qs
 
-def app(environ, start_response):
-    query = urlparse.urlparse(environ['QUERY_STRING'])[4]
-    query_items = query.split("&")
+
+def application(environ, start_response):
+    query = parse_qs(environ['QUERY_STRING'], keep_blank_values=True)
+    body = []
+    for key, value in query.items():
+        for item in value:
+            body.append(key + "=" + item + "\r\n")
+
     status = '200 OK'
-    response_headers = [
-        ('Content-type', 'text/plain')
+    headers = [
+        ('Content-Type', 'text/plain')
     ]
-    body = ''
-    for i in query_items:
-        body += i + '\r\n'
-    start_response(status, response_headers)
-    return [body]
+
+    start_response(status, headers)
+    return body
