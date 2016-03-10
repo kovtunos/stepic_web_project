606bc4096e5a130e4cdc4aae7fcff48aaab30c13
Andrey Kovtun <kovtunos@openmailbox.org>
2016-03-10 08:50:14 +0300

    Create MySQL database and user.

--- init.sh
+++ init.sh
@@ -12,3 +12,8 @@ sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn_hello.conf /etc/gunicorn.d/test
 sudo ln -sf /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart
+
+# MySQL
+sudo service mysql restart
+sudo mysql -uroot -e "CREATE DATABASE qa CHARACTER SET utf8 COLLATE utf8_general_ci;"
+sudo mysql -uroot -e "GRANT ALL PRIVILEGES ON qa.* TO 'qa_user'@'localhost' IDENTIFIED BY '123456789';"
