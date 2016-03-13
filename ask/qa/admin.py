2119dcf88b3bd854e202da2e865476198e6b6bb1
Andrey Kovtun <kovtunos@openmailbox.org>
2016-03-13 14:13:28 +0300

    Install autofixture module, add models to admin page.

--- ask/qa/admin.py
+++ ask/qa/admin.py
@@ -1,1 +1,17 @@
 from django.contrib import admin
+from .models import Question, Answer
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'text', 'added_at', 'rating', 'author', 'likes')
+    date_hierarchy = 'added_at'
+
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('text', 'added_at', 'author')
+    date_hierarchy = 'added_at'
+    raw_id_fields = ('question',)
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer, AnswerAdmin)
