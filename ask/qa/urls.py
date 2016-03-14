47f6c4947b56d11a131c73271e85bfe891b3afdd
Andrey Kovtun <kovtunos@openmailbox.org>
2016-03-14 09:13:58 +0300

    Implement sort by rating.

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from qa.views import test, question_list, question_detail
+from qa.views import test, question_list, question_detail, popular
 
 urlpatterns = patterns('',
     url(r'^$', question_list, name='question_list'),
     url(r'^question/(?P<pk>\d+)/', question_detail, name='question_detail'),
+    url(r'^popular/', popular, name='popular'),
     url(r'^login/', test, name='login'),
     url(r'^signup/', test, name='signup'),
     url(r'^ask/', test, name='ask'),
-    url(r'^popular/', test, name='popular'),
     url(r'^new/', test, name='new'),
     url(r'^admin/', admin.site.urls),
 )
--- ask/qa/views.py
+++ ask/qa/views.py
@@ -9,13 +9,33 @@ def test(request, *args, **kwargs):
 
 
 def question_list(request):
+    # TODO: use class-based views
     questions = Question.objects.all()
+    questions = Question.objects.order_by('-added_at')
     limit = request.GET.get('limit', 10)
     page = request.GET.get('page', 1)
     paginator = Paginator(questions, limit)
+    # TODO: remove hardcode
     paginator.baseurl = '/?page='
     page = paginator.page(page)
     return render(request, 'qa/question/list.html', {
+        'questions': page.object_list,
+        'page': page,
+        'paginator': paginator,
+    })
+
+
+def popular(request):
+    # TODO: use class-based views
+    questions = Question.objects.all()
+    questions = Question.objects.order_by('-rating')
+    limit = request.GET.get('limit', 10)
+    page = request.GET.get('page', 1)
+    paginator = Paginator(questions, limit)
+    # TODO: remove hardcode
+    paginator.baseurl = '/popular/?page='
+    page = paginator.page(page)
+    return render(request, 'qa/question/list_rating.html', {
         'questions': page.object_list,
         'page': page,
         'paginator': paginator,
