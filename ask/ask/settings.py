bb4cf4f4057cfd1ab8f4cae1a543ed77140261c7
Andrey Kovtun <kovtunos@openmailbox.org>
2016-03-16 23:39:12 +0300

    Switch to MySQL.

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -57,23 +57,23 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'ask',
-#         'USER': 'ask_user',
-#         'PASSWORD': '123456789',
-#         'HOST': '',
-#         'PORT': '3306',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ask',
+        'USER': 'ask_user',
+        'PASSWORD': '123456789',
+        'HOST': '',
+        'PORT': '3306',
+    }
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
--- init.sh
+++ init.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+# Install
 sudo pip install django-autofixture pytz
+sudo apt-get install -y w3m
 
+# Nginx
 if [ -f /etc/nginx/sites-enabled/default ]; then
   sudo rm /etc/nginx/sites-enabled/default
 fi
-
-# Nginx
 touch /home/box/nginx.log
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/ask.conf
 sudo /etc/init.d/nginx restart
@@ -15,6 +16,12 @@ sudo /etc/init.d/nginx restart
 touch /home/box/gunicorn.log
 sudo ln -sf /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart
+
+# MySQL
+echo 'innodb_use_native_aio = 0' | sudo tee --append /etc/mysql/my.cnf
+sudo service mysql restart
+sudo mysql -uroot -e "CREATE DATABASE ask CHARACTER SET utf8 COLLATE utf8_general_ci;"
+sudo mysql -uroot -e "GRANT ALL PRIVILEGES ON ask.* TO 'ask_user'@'localhost' IDENTIFIED BY '123456789';"
 
 echo 'syncing db...'
 python /home/box/web/ask/manage.py syncdb
